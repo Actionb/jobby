8512007ed458938f492dc77673b0d744c91a8472
Actionb <yummytea1@gmail.com>
2024-05-27 17:29:13 +0200

    Add SucheModel

---
+++ jobby/app/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+class SucheModel(models.Model):
+    class AngebotsChoices(models.IntegerChoices):
+        ARBEIT = 1, "Arbeit"
+        SELBSTÄNDIGKEIT = 2, "Selbständigkeit"
+        AUSBILDUNG = 4, "Ausbildung/Duales Studium"
+        PRAKTIKUM = 34, "Praktikum/Trainee"
+
+    class BefristungChoices(models.IntegerChoices):
+        BEFRISTET = 1, "befristet"
+        UNBEFRISTET = 2, "unbefristet"
+
+    class ArbeitszeitChoices(models.TextChoices):
+        VOLLZEIT = "vz", "Vollzeit"
+        TEILZEIT = "tz", "Teilzeit"
+        SCHICHT = "snw", "Schicht/Nachtarbeit/Wochenende"
+        HOME_OFFICE = "ho", "Heim-/Telearbeit"
+        MINIJOB = "mj", "Minijob"
+
+    was = models.CharField(max_length=100)
+    wo = models.CharField(max_length=100)  # TODO: required?
+    berufsfeld = models.CharField(max_length=100, blank=True, null=True)
+    arbeitgeber = models.CharField(max_length=100, blank=True, null=True)
+    published_since = models.PositiveSmallIntegerField(blank=True, null=True)
+    zeitarbeit = models.NullBooleanField(blank=True, null=True)
+    # TODO: null=True on IntegerField a problem?
+    angebot_art = models.IntegerField(choices=AngebotsChoices, blank=True, null=True)
+    befristung = models.IntegerField(choices=BefristungChoices, blank=True, null=True)
+    arbeitszeit = models.CharField(max_length=3, choices=ArbeitszeitChoices, blank=True, null=True)
+    behinderung = models.BooleanField(default=False)
+    corona = models.BooleanField(default=False)  # TODO: might be out-dated
+    umfeld = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    # Query specific options
+    page = models.PositiveSmallIntegerField()
+    size = models.PositiveSmallIntegerField()
