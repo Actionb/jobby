ed4fae7078d3550fab4d0af121b8cc332cbfb86f
Actionb <yummytea1@gmail.com>
2024-06-05 15:09:52 +0200

    Add django debug toolbar

--- project/settings_dev.py
+++ project/settings_dev.py
@@ -1,3 +1,16 @@
 from .settings import *  # noqa
 
 DEBUG = True
+
+ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]"]
+
+INSTALLED_APPS += [  # noqa
+    "debug_toolbar",
+]
+
+MIDDLEWARE += [  # noqa
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+# Required for debug_toolbar:
+INTERNAL_IPS = ["127.0.0.1"]
--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,3 +24,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("jobby.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
