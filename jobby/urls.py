74dd80caf4c4d619f1b7bca1e56f3f00c7e33c8e
Actionb <yummytea1@gmail.com>
2024-06-06 09:58:34 +0200

    Stellenangebot view URL conf

--- jobby/urls.py
+++ jobby/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from jobby.views import SucheView, WatchlistView, watchlist_toggle
+from jobby.views import StellenangebotView, SucheView, WatchlistView, watchlist_toggle
 
 urlpatterns = [
     path("", SucheView.as_view(), name="suche"),
     path("watchlist/", WatchlistView.as_view(), name="watchlist"),
     path("watchlist/toggle/", watchlist_toggle, name="watchlist_toggle"),
+    path("angebot/", StellenangebotView.as_view(extra_context={"add": True}), name="stellenangebot_add"),
+    path("angebot/<int:id>/", StellenangebotView.as_view(extra_context={"add": False}), name="stellenangebot_edit"),
 ]
