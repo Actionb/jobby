6693ae88b391e38dc11e8737a5815d9fed4db8a5
Actionb <yummytea1@gmail.com>
2024-06-06 11:47:16 +0200

    Render SucheForm boolean fields as selects

--- jobby/forms.py
+++ jobby/forms.py
@@ -2,14 +2,19 @@ from django import forms
 
 from jobby.models import Stellenangebot, SucheModel
 
+null_boolean_select = forms.Select(choices=[(None, "---------"), (True, "Ja"), (False, "Nein")])
+
 
 class SucheForm(forms.ModelForm):
     class Meta:
         model = SucheModel
         fields = forms.ALL_FIELDS
+        exclude = ["corona"]
         widgets = {
             "page": forms.HiddenInput,
             "size": forms.HiddenInput,
+            "zeitarbeit": null_boolean_select,
+            "behinderung": null_boolean_select,
         }
 
 
