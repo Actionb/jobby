dcc03f9cd179f411b435578f411180de92d85d05
Actionb <yummytea1@gmail.com>
2024-06-10 14:14:11 +0200

    Add test docstrings

--- tests/test_models.py
+++ tests/test_models.py
@@ -28,6 +28,10 @@ def other(refnr):
 
 
 def test_update_stellenangebot(stellenangebot, other):
+    """
+    Assert that ``_update_stellenangebot`` updates the given Stellenangebot
+    instance with the data from the other.
+    """
     assert _update_stellenangebot(stellenangebot, other)
     stellenangebot.refresh_from_db()
     assert stellenangebot.titel == other.titel
@@ -35,11 +39,19 @@ def test_update_stellenangebot(stellenangebot, other):
 
 
 def test_update_stellenangebot_identical(stellenangebot, django_assert_num_queries):
+    """
+    Assert that ``_update_stellenangebot`` does not perform any queries if the
+    two Stellenangebot arguments are identical.
+    """
     with django_assert_num_queries(0):
         assert not _update_stellenangebot(stellenangebot, stellenangebot)
 
 
 def test_update_stellenangebot_different_refnr(stellenangebot, other, django_assert_num_queries):
+    """
+    Assert that ``_update_stellenangebot`` does not perform any queries if the
+    two Stellenangebot do not have the same refnr.
+    """
     other.refnr = "1"
     with django_assert_num_queries(0):
         assert not _update_stellenangebot(stellenangebot, other)
@@ -52,18 +64,21 @@ class AsDictTestModel(models.Model):
 
 
 def test_as_dict():
+    """Assert that ``as_dict`` returns the expected result."""
     instance = AsDictTestModel(name="test")
     expected = {"id": None, "name": "test", "empty": None, "default": "default"}
     assert _as_dict(instance, empty=True, default=True) == expected
 
 
 def test_as_dict_ignores_empty_values():
+    """Assert that ``as_dict`` filters out empty values if empty argument is False."""
     instance = AsDictTestModel(name="test", default="not default")
     expected = {"name": "test", "default": "not default"}
     assert _as_dict(instance, empty=False) == expected
 
 
 def test_as_dict_ignores_default_values():
+    """Assert that ``as_dict`` filters out values that are a field's default."""
     instance = AsDictTestModel(name="test", empty="not empty")
     expected = {"name": "test", "empty": "not empty"}
     assert _as_dict(instance, default=False) == expected
@@ -79,9 +94,17 @@ urlpatterns = [
 class TestStellenangebot:
 
     def test_as_url_saved_instance(self, stellenangebot):
+        """
+        Assert that ``as_url`` returns the URL to the instance's edit page if
+        the instance is saved.
+        """
         assert stellenangebot.as_url() == f"/foo/{stellenangebot.pk}"
 
     def test_as_url_unsaved_instance(self):
+        """
+        Assert that ``as_url`` returns the URL to the add page if the instance
+        is not saved.
+        """
         stellenangebot = StellenangebotFactory.build()
         with patch("jobby.models._as_dict", new=Mock(return_value={"titel": "foo", "refnr": 1234})):
             assert stellenangebot.as_url() == "/foo/?titel=foo&refnr=1234"
@@ -90,19 +113,35 @@ class TestStellenangebot:
 class TestWatchlist:
 
     def test_add_watchlist_item(self, watchlist, stellenangebot):
+        """
+        Assert that ``add_watchlist_item`` adds the given Stellenangebot
+        instance to the watchlist.
+        """
         added = watchlist.add_watchlist_item(stellenangebot)
         assert added
         assert watchlist.items.filter(stellenangebot=stellenangebot).exists()
 
     def test_add_watchlist_item_already_on_watchlist(self, watchlist, watchlist_item, stellenangebot):
+        """
+        Assert that ``add_watchlist_item`` does not add the given Stellenangebot
+        instance to the watchlist if it is already on the watchlist.
+        """
         added = watchlist.add_watchlist_item(stellenangebot)
         assert not added
         assert watchlist.items.filter(stellenangebot=stellenangebot).exists()
 
     def test_remove_watchlist_item(self, watchlist, watchlist_item, stellenangebot):
+        """
+        Assert that ``remove_watchlist_item`` removes the given Stellenangebot
+        instance from the watchlist.
+        """
         watchlist.remove_watchlist_item(stellenangebot)
         assert not watchlist.items.filter(stellenangebot=stellenangebot).exists()
 
     def test_get_stellenangebote(self, watchlist, watchlist_item, stellenangebot, django_assert_num_queries):
+        """
+        Assert that ``get_stellenangebote`` returns the Stellenangebot
+        instances that are on the watchlist.
+        """
         with django_assert_num_queries(1):
             assert stellenangebot in watchlist.get_stellenangebote()
--- tests/test_views.py
+++ tests/test_views.py
@@ -17,11 +17,13 @@ pytestmark = [pytest.mark.django_db]
 
 @pytest.fixture
 def search_results():
+    """Return a list of integers to act as search results."""
     return list(range(1, 11))
 
 
 @pytest.fixture
 def search_response_mock(search_results):
+    """Return a mock object to act as the response of a search request."""
     return Mock(
         results=search_results,
         result_count=len(search_results),
@@ -30,6 +32,7 @@ def search_response_mock(search_results):
 
 @pytest.fixture
 def search_mock(search_response_mock):
+    """Mock the ``search`` function to return a mocked response."""
     with patch("jobby.views.search") as m:
         m.return_value = search_response_mock
         yield m
@@ -37,11 +40,13 @@ def search_mock(search_response_mock):
 
 @pytest.fixture
 def cleaned_data():
+    """The cleaned data of the mocked search form."""
     return {}
 
 
 @pytest.fixture
 def form_mock(cleaned_data):
+    """Return a mock with cleaned_data."""
     return Mock(cleaned_data=cleaned_data)
 
 
@@ -59,25 +64,30 @@ class TestSucheView:
 
     @pytest.fixture
     def context_data(self):
+        """Return the context data for the get_context_data mock."""
         return {}
 
     @pytest.fixture(autouse=True)
     def get_context_data_mock(self, view, context_data):
+        """Mock out the view's ``get_context_data`` method."""
         with patch.object(view, "get_context_data") as m:
             m.return_value = context_data
             yield m
 
     @pytest.fixture(autouse=True)
     def render_mock(self, view):
+        """Mock out the view's ``render_to_response`` method."""
         with patch.object(view, "render_to_response") as m:
             yield m
 
     @pytest.fixture
     def form_is_valid(self):
+        """Return whether the mocked form class is valid."""
         return True
 
     @pytest.fixture(autouse=True)
     def form_class_mock(self, view, form_is_valid):
+        """Replace the view's ``form_class`` with a Mock."""
         with patch.object(view, "form_class") as m:
             m.return_value.is_valid.return_value = form_is_valid
             yield m
@@ -85,6 +95,7 @@ class TestSucheView:
     @pytest.mark.parametrize("request_data", [{"suche": ""}])
     @pytest.mark.parametrize("form_is_valid", [True])
     def test_get_form_valid(self, view, http_request, request_data, form_is_valid):
+        """Assert that get() calls ``form_valid`` if the form is valid."""
         with patch.object(view, "form_valid") as form_valid_mock:
             view.get(http_request)
 
@@ -93,6 +104,7 @@ class TestSucheView:
     @pytest.mark.parametrize("request_data", [{"suche": ""}])
     @pytest.mark.parametrize("form_is_valid", [False])
     def test_get_form_invalid(self, view, http_request, request_data, form_is_valid):
+        """Assert that get() calls ``form_invalid`` if the form is invalid."""
         with patch.object(view, "form_invalid") as form_invalid_mock:
             view.get(http_request)
 
@@ -100,12 +112,17 @@ class TestSucheView:
 
     @pytest.mark.parametrize("request_data", [{}])
     def test_get_calls_super_if_suche_not_in_request_data(self, view, http_request, request_data):
+        """
+        Assert that get() calls the super method if the request data does not
+        contain a ``suche`` key.
+        """
         with patch("jobby.views.super") as super_mock:
             view.get(http_request)
 
         super_mock.assert_called()
 
     def test_form_valid(self, view, search_mock, form_mock, render_mock, search_results, search_response_mock):
+        """Assert that form_valid() calls ``render_to_response``."""
         with patch.object(view, "get_pagination_context") as pagination_context_mock:
             with patch.object(view, "get_results_context") as results_context_mock:
                 results_context_mock.return_value = {}
@@ -118,6 +135,10 @@ class TestSucheView:
 
     @pytest.mark.parametrize("search_results", [[]])
     def test_form_valid_no_results(self, view, search_mock, form_mock, search_results):
+        """
+        Assert ``get_pagination_context`` is not called if the search did not
+        return results.
+        """
         with patch.object(view, "get_pagination_context") as pagination_context_mock:
             pagination_context_mock.return_value = {}
             view.form_valid(form_mock)
@@ -125,17 +146,25 @@ class TestSucheView:
         pagination_context_mock.assert_not_called()
 
     def test_user_message_on_exception(self, view, search_mock, form_mock):
+        """
+        Assert that form_valid calls ``_send_error_message`` if the search
+        raised an exception.
+        """
         search_mock.side_effect = Exception("A test exception has occurred.")
         with patch.object(view, "_send_error_message") as send_message_mock:
             view.form_valid(form_mock)
             send_message_mock.assert_called()
 
     def test_get_watchlisted_ids(self, view, watchlist_item, stellenangebot):
+        """
+        Assert that ``_get_watchlisted_ids`` returns the ids of the
+        Stellenangebot instances that have been watchlisted.
+        """
         assert view._get_watchlisted_ids(results=[stellenangebot]) == {stellenangebot.pk}
 
     def test_get_watchlisted_ids_filter(self, view, stellenangebot):
         """
-        Assert that _get_watchlisted_ids filters only with saved model
+        Assert that ``_get_watchlisted_ids`` filters only with saved model
         instances.
         """
         with patch("jobby.views.WatchlistItem") as watchlist_item_mock:
@@ -146,6 +175,7 @@ class TestSucheView:
             filter_mock.assert_called_with(stellenangebot__in=[stellenangebot])
 
     def test_get_watchlisted_ids_no_results(self, view):
+        # TODO: test that _get_watchlisted_ids returns an empty set here
         with patch("jobby.views.WatchlistItem") as watchlist_item_mock:
             none_mock = Mock()
             none_mock.return_value.values_list.return_value = []
@@ -154,6 +184,7 @@ class TestSucheView:
             none_mock.assert_called()
 
     def test_get_results_context(self, view, search_response_mock, stellenangebot):
+        """Assert that ``get_results_context`` returns the expected data."""
         new = StellenangebotFactory.build()
         search_response_mock.results = [stellenangebot, new]
         search_response_mock.result_count = 2
@@ -164,6 +195,7 @@ class TestSucheView:
 
     @pytest.mark.parametrize("request_data", [{PAGE_VAR: "2"}])
     def test_get_pagination_context(self, view, request_data):
+        """Assert that ``get_pagination_context`` returns the expected data."""
         ctx = view.get_pagination_context(result_count=11, per_page=1)
         assert ctx["current_page"] == 2
         assert ctx["page_range"] == [1, 2, 3, 4, 5, Paginator.ELLIPSIS, 10, 11]
@@ -171,6 +203,10 @@ class TestSucheView:
 
     @pytest.mark.parametrize("request_data", [{PAGE_VAR: "foo"}])
     def test_get_pagination_context_invalid_page(self, view, request_data):
+        """
+        Assert that ``get_pagination_context`` sets the current page to 1 if
+        the PAGE_VAR of the request is not a valid integer.
+        """
         ctx = view.get_pagination_context(result_count=1)
         assert ctx["current_page"] == 1
 
@@ -183,31 +219,38 @@ class TestWatchlistView:
 
     @pytest.fixture
     def request_data(self, watchlist_name):
+        """Return the data for the request."""
         return {"watchlist_name": watchlist_name}
 
     @pytest.fixture
     def current_mock(self, view):
+        """Mock out the view's ``current_watchlist_name`` method."""
         with patch.object(view, "current_watchlist_name") as m:
             yield m
 
     @pytest.fixture
     def watchlist_names_mock(self, view):
+        """Mock out the view's ``get_watchlist_names`` method."""
         with patch.object(view, "get_watchlist_names") as m:
             yield m
 
     @pytest.mark.parametrize("request_data", [{"watchlist_name": "foo"}])
     def test_current_watchlist_name(self, view, http_request, request_data):
+        """Assert that ``current_watchlist_name`` returns the expected data."""
         assert view.current_watchlist_name(http_request) == "foo"
 
     def test_get_watchlist(self, view, http_request, watchlist):
+        """Assert that ``get_watchlist`` returns the expected Watchlist instance."""
         assert view.get_watchlist(http_request) == watchlist
 
     def test_get_queryset(self, view, watchlist, watchlist_item, stellenangebot):
+        """Assert that ``get_queryset`` returns the expected queryset."""
         queryset = view.get_queryset()
         assert isinstance(queryset, QuerySet)
         assert stellenangebot in queryset
 
     def test_get_watchlist_names(self, view, watchlist, watchlist_name):
+        """Assert that ``get_watchlist_names`` returns the expected list of names."""
         assert list(view.get_watchlist_names()) == [watchlist_name]
 
     def test_creates_watchlist_if_does_not_exist(self, view, client):
@@ -227,15 +270,24 @@ class TestWatchlistToggle:
 
     @pytest.fixture
     def request_data(self, stellenangebot, watchlist_name):
+        """Return the data for the request."""
         return {"titel": stellenangebot.titel, "refnr": stellenangebot.refnr, "watchlist_name": watchlist_name}
 
     def test_watchlist_toggle_not_on_watchlist(self, post_request, watchlist, stellenangebot):
+        """
+        Assert that ``watchlist_toggle`` adds a Stellenangebot instance to the
+        watchlist if it is not already on it.
+        """
         response = watchlist_toggle(post_request)
         assert response.status_code == 200
         assert json.loads(response.content)["on_watchlist"]
         assert watchlist.items.filter(stellenangebot=stellenangebot).exists()
 
     def test_watchlist_toggle_already_on_watchlist(self, post_request, watchlist, watchlist_item, stellenangebot):
+        """
+        Assert that ``watchlist_toggle`` removes a Stellenangebot instance from
+        the watchlist if it is already on it.
+        """
         response = watchlist_toggle(post_request)
         assert response.status_code == 200
         assert not json.loads(response.content)["on_watchlist"]
@@ -243,6 +295,10 @@ class TestWatchlistToggle:
 
     @pytest.mark.parametrize("stellenangebot", [StellenangebotFactory.build()])
     def test_watchlist_toggle_new_angebot(self, post_request, watchlist, stellenangebot):
+        """
+        Assert that ``watchlist_toggle`` creates a Stellenangebot instance if
+        no Stellenangebot with the requested refnr exists.
+        """
         response = watchlist_toggle(post_request)
         assert response.status_code == 200
         assert json.loads(response.content)["on_watchlist"]
@@ -252,16 +308,25 @@ class TestWatchlistToggle:
 
     @pytest.mark.parametrize("request_data", [{}])
     def test_watchlist_toggle_no_refnr(self, post_request, request_data):
+        """
+        Assert that ``watchlist_toggle`` returns a response with status code
+        400 if the no refnr was included in the request data.
+        """
         response = watchlist_toggle(post_request)
         assert response.status_code == 400
 
     @pytest.fixture
     def search_result_form_mock(self):
+        """Mock the StellenangebotForm."""
         with patch("jobby.views.StellenangebotForm") as m:
             yield m
 
     @pytest.mark.parametrize("stellenangebot", [StellenangebotFactory.build()])
     def test_watchlist_toggle_new_angebot_form_invalid(self, post_request, search_result_form_mock, stellenangebot):
+        """
+        Assert that ``watchlist_toggle`` returns a response with status code
+        400 if form validation fails.
+        """
         search_result_form_mock.return_value.is_valid.return_value = False
         response = watchlist_toggle(post_request)
         assert response.status_code == 400
@@ -275,10 +340,15 @@ class TestStellenangebotView:
 
     @pytest.mark.parametrize("view_extra_context", [{"add": True}])
     def test_get_object_add(self, view, view_extra_context):
+        """Assert that ``get_object`` returns None if this is an 'add' view."""
         assert view.get_object() is None
 
     @pytest.mark.parametrize("view_extra_context", [{"add": False}])
     def test_get_object_edit(self, view, view_extra_context, super_mock):
+        """
+        Assert that ``get_object`` calls the super method if this is an 'edit'
+        view.
+        """
         get_object_mock = Mock()
         super_mock.return_value.get_object = get_object_mock
         view.get_object()
@@ -288,8 +358,8 @@ class TestStellenangebotView:
     @pytest.mark.parametrize("view_extra_context", [{"add": True}])
     def test_get_initial_add(self, view, view_extra_context, super_mock, request_data):
         """
-        Assert that get_initial includes the request's data if the view is an
-        'add' view.
+        Assert that ``get_initial`` includes the request's GET data if the view
+        is an 'add' view.
         """
         super_mock.return_value.get_initial.return_value = {"foo": "bar"}
         assert view.get_initial() == {"foo": "bar", **request_data}
@@ -297,11 +367,17 @@ class TestStellenangebotView:
     @pytest.mark.parametrize("request_data", [{"titel": "foo"}])
     @pytest.mark.parametrize("view_extra_context", [{"add": False}])
     def test_get_initial_edit(self, view, view_extra_context, super_mock, request_data):
+        """
+        Assert that ``get_initial`` does *not* include the request's GET data if
+        the view is an 'edit' view.
+        """
         super_mock.return_value.get_initial.return_value = {"foo": "bar"}
         assert view.get_initial() == {"foo": "bar"}
 
     @pytest.fixture
     def super_get_mock(self, view, super_mock):
+        """Mock out super().get() to return a mocked response."""
+
         def get(*_args, **_kwargs):
             view.object = None
             return response_mock
@@ -313,8 +389,8 @@ class TestStellenangebotView:
     @pytest.mark.parametrize("view_extra_context", [{"add": True}])
     def test_get_add_refnr_exists(self, view, http_request, stellenangebot, super_get_mock, view_extra_context):
         """
-        Assert that get returns a redirect to an edit page if a Stellenangebot
-        with the given refnr exists.
+        Assert that ``get`` returns a redirect to an edit page if a
+        Stellenangebot with the given refnr exists.
         """
         http_request.GET = {"refnr": stellenangebot.refnr}
         response = view.get(http_request)
@@ -324,7 +400,7 @@ class TestStellenangebotView:
     @pytest.mark.parametrize("view_extra_context", [{"add": True}])
     def test_get_add_refnr_does_not_exist(self, view, http_request, super_get_mock, view_extra_context):
         """
-        Assert that get returns the response from super().get() if no
+        Assert that ``get`` returns the response from super().get() if no
         Stellenangebot instance with the given refnr exists.
         """
         http_request.GET = {"refnr": "1234"}
@@ -334,8 +410,8 @@ class TestStellenangebotView:
     @pytest.mark.parametrize("view_extra_context", [{"add": True}])
     def test_get_add_no_refnr(self, view, http_request, super_get_mock, view_extra_context):
         """
-        Assert that get returns the response from super().get() if the request
-        data does not contain a refnr.
+        Assert that ``get`` returns the response from super().get() if the
+        request data does not contain a refnr.
         """
         http_request.GET = {}
         response = view.get(http_request)
@@ -344,20 +420,36 @@ class TestStellenangebotView:
     @pytest.mark.parametrize("request_data", [{"refnr": "1234"}])
     @pytest.mark.parametrize("view_extra_context", [{"add": True}])
     def test_get_arge_link_request_has_refnr(self, view, view_extra_context, request_data):
-        assert view.get_arge_link()
+        """
+        Assert that ``get_arge_link`` returns a URL to the details page of the
+        refnr given in the 'add' view's request data.
+        """
+        assert view.get_arge_link() == f"https://www.arbeitsagentur.de/jobsuche/jobdetail/{request_data["refnr"]}"
 
     @pytest.mark.parametrize("request_data", [{}])
     @pytest.mark.parametrize("view_extra_context", [{"add": True}])
     def test_get_arge_link_request_does_not_have_refnr(self, view, view_extra_context, request_data):
+        """
+        Assert that ``get_arge_link`` returns None if the request data for an
+        'add' view does not contain a refnr.
+        """
         assert not view.get_arge_link()
 
     @pytest.mark.parametrize("view_extra_context", [{"add": False}])
-    def test_get_arge_link_edit(self, view, view_extra_context, stellenangebot):
+    def test_get_arge_link_edit(self, view, view_extra_context, stellenangebot, refnr):
+        """
+        Assert that ``get_arge_link`` returns the URL to the details page of the
+        edit view's Stellenangebot instance.
+        """
         view.object = stellenangebot
-        assert view.get_arge_link()
+        assert view.get_arge_link() == f"https://www.arbeitsagentur.de/jobsuche/jobdetail/{refnr}"
 
     @pytest.mark.parametrize("refnr", [""])
     @pytest.mark.parametrize("view_extra_context", [{"add": False}])
     def test_get_arge_link_edit_no_refnr(self, view, view_extra_context, stellenangebot, refnr):
+        """
+        Assert that ``get_arge_link`` returns None if the edit view's instance
+        does not have a refnr set.
+        """
         view.object = stellenangebot
         assert not view.get_arge_link()
