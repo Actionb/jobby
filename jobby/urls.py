4e3cf472847f08d9c365377ea4859ac5e249b344
Actionb <yummytea1@gmail.com>
2024-06-11 11:35:30 +0200

    Add URLs for remove views

--- jobby/urls.py
+++ jobby/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 
-from jobby.views import StellenangebotView, SucheView, WatchlistView, watchlist_toggle
+from jobby.views import (
+    StellenangebotView,
+    SucheView,
+    WatchlistView,
+    watchlist_remove,
+    watchlist_remove_all,
+    watchlist_toggle,
+)
 
 urlpatterns = [
     path("", SucheView.as_view(), name="suche"),
     path("watchlist/", WatchlistView.as_view(), name="watchlist"),
     path("watchlist/toggle/", watchlist_toggle, name="watchlist_toggle"),
+    path("watchlist/remove/", watchlist_remove, name="watchlist_remove"),
+    path("watchlist/remove_all/", watchlist_remove_all, name="watchlist_remove_all"),
     path("angebot/", StellenangebotView.as_view(extra_context={"add": True}), name="stellenangebot_add"),
     path("angebot/<int:id>/", StellenangebotView.as_view(extra_context={"add": False}), name="stellenangebot_edit"),
 ]
