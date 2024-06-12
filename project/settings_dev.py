5efd6ac5ca0e9443cc22d857c21c926d5fe2b3f1
Actionb <yummytea1@gmail.com>
2024-06-12 22:07:34 +0200

    Use postgres for production database

--- project/settings_dev.py
+++ project/settings_dev.py
@@ -1,6 +1,18 @@
 from .settings import *  # noqa
 
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]"]
 
