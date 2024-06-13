851d5a24b3f61f22133d8678e21ddfc0ae61b1ee
Actionb <yummytea1@gmail.com>
2024-06-13 16:24:41 +0200

    Move settings into dedicated package

--- project/settings/base.py
+++ project/settings/base.py
@@ -10,47 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Read secrets from files
-try:
-    with open(BASE_DIR / ".secrets" / ".passwd") as f:
-        password = f.readline().strip()
-except FileNotFoundError as e:
-    raise FileNotFoundError(
-        "No database password file found. Create a file called '.passwd' "
-        "in the '.secrets' subdirectory that contains the database password.\n"
-        "HINT: run setup.sh"
-    ) from e
-
-try:
-    with open(BASE_DIR / ".secrets" / ".key") as f:
-        SECRET_KEY = f.readline().strip()
-except FileNotFoundError as e:
-    raise FileNotFoundError(
-        "No secret key file found. Create a file called '.key' "
-        "in the '.secrets' subdirectory that contains the secret key.\n"
-        "HINT: run setup.sh"
-    ) from e
-
-try:
-    with open(BASE_DIR / ".secrets" / ".allowedhosts") as f:
-        ALLOWED_HOSTS = f.readline().strip().split(",")
-except FileNotFoundError as e:
-    raise FileNotFoundError(
-        "No allowed hosts file found. Create a file called '.allowedhosts' "
-        "in the '.secrets' subdirectory that contains a list of allowed "
-        "host names.\n"
-        "HINT: run setup.sh"
-    ) from e
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -94,20 +53,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "project.wsgi.application"
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "jobby"),
-        "USER": os.environ.get("DB_USER", "jobby"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", 5432),
-        "PASSWORD": password,
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
