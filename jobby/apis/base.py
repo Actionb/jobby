99b48ea170c9a14c7eb0b6d1832167330bd68efd
Actionb <yummytea1@gmail.com>
2024-06-20 17:57:15 +0200

    Make SearchResponse abstract

--- jobby/apis/base.py
+++ jobby/apis/base.py
@@ -4,8 +4,6 @@ from functools import cached_property
 
 from django.utils.timezone import make_aware
 
-from jobby.models import Stellenangebot, _update_stellenangebot
-
 
 class BaseAPI(ABC):
 
@@ -13,7 +11,7 @@ class BaseAPI(ABC):
     def search(self) -> "SearchResponse": ...
 
 
-class SearchResponse:
+class SearchResponse(ABC):
 
     def __init__(self, response):
         self.response = response
@@ -33,70 +31,13 @@ class SearchResponse:
 
     @property
     def has_results(self):
-        # FIXME: some queries return responses without "stellenangebote" data
-        #  (f.ex. Angebotsart=Ausbildung)
-        return self._get_total_result_count(self.data) > 0 and "stellenangebote" in self.data
+        return self._get_total_result_count(self.data) > 0
 
-    def _get_results(self, data):
-        if not self.has_results:
-            return []
+    @abstractmethod
+    def _get_total_result_count(self, data) -> int: ...
 
-        # Parse each search result, and collect the ref numbers for database
-        # lookups.
-        angebote = self._process_results(data["stellenangebote"])
-        refs = set(s.refnr for s in angebote)
-
-        # Create the list of Stellenangebot instances.
-        # Use saved Stellenangebot instances whenever possible. Update
-        # saved instances if the data has changed.
-        results = []
-        existing = self._get_existing(refs)
-        existing_refs = set(existing.values_list("refnr", flat=True))
-        for angebot in angebote:
-            if angebot.refnr in existing_refs:
-                stellenangebot = existing.get(refnr=angebot.refnr)
-                _update_stellenangebot(stellenangebot, angebot)
-            else:
-                stellenangebot = angebot
-            results.append(stellenangebot)
-        return results
-
-    def _process_results(self, results):
-        """
-        Walk through the dictionaries of search results and return them as
-        Stellenangebot instances.
-        """
-        # TODO: include "externeUrl" data that is present on some results
-        processed = []
-        # TODO: use SearchResultForm here?
-        for result in results:
-            instance = Stellenangebot(
-                titel=result.get("titel", ""),
-                refnr=result.get("refnr", ""),
-                beruf=result.get("beruf", ""),
-                arbeitgeber=result.get("arbeitgeber", ""),
-                arbeitsort=self._parse_arbeitsort(result.get("arbeitsort", {})),
-                # TODO: parse into date and datetime:
-                eintrittsdatum=result.get("eintrittsdatum", ""),
-                veroeffentlicht=result.get("aktuelleVeroeffentlichungsdatum", ""),
-                modified=self._make_aware(result.get("modifikationsTimestamp", "")),
-                externe_url=result.get("externeUrl", ""),
-            )
-            processed.append(instance)
-        return processed
-
-    @staticmethod
-    def _parse_arbeitsort(arbeitsort_dict):
-        """
-        Return a string for the 'arbeitsort' field from the given data from a
-        search result.
-        """
-        ort = arbeitsort_dict.get("ort", "")
-        plz = arbeitsort_dict.get("plz", "")
-        if plz:
-            return f"{ort}, {plz}"
-        else:
-            return ort
+    @abstractmethod
+    def _get_results(self, data) -> list: ...
 
     @staticmethod
     def _make_aware(datetime_string):
@@ -105,11 +46,3 @@ class SearchResponse:
             return make_aware(datetime.fromisoformat(datetime_string))
         except ValueError:
             return ""
-
-    @staticmethod
-    def _get_existing(refs):
-        return Stellenangebot.objects.filter(refnr__in=refs)
-
-    @staticmethod
-    def _get_total_result_count(data):
-        return data.get("maxErgebnisse", 0)
