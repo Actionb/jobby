a9e54560d2165b206ffac0eea55b8112c8a13a59
Actionb <yummytea1@gmail.com>
2024-06-21 16:16:31 +0200

    Add add_search_filters template tag

--- jobby/templatetags/jobby.py
+++ jobby/templatetags/jobby.py
@@ -1,13 +1,59 @@
-from django import template
-from django.utils.http import urlencode
+from urllib.parse import parse_qsl, unquote, urlparse, urlunparse
 
-from jobby.views import PAGE_VAR
+from django import template
+from django.urls import Resolver404, get_script_prefix, resolve
+from django.utils.http import urlencode
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def paginator_url(context, page_number):
+    from jobby.views import PAGE_VAR  # avoid circular import
+
     params = dict(context["request"].GET.items())
     params[PAGE_VAR] = page_number
     return "?%s" % urlencode(sorted(params.items()))
+
+
+@register.simple_tag(takes_context=True)
+def add_search_filters(context, url):
+    """
+    Add the preserved search form parameters to the given url.
+
+    When navigating away from a search page, the search parameters will be
+     added to the query string like this: '_search_filters=was%3DFoo%26wo%3DBar'
+
+    When navigating back to the search page, the filters are recovered from the
+    above query string parameter:
+        '_search_filters=was%3DFoo%26wo%3DBar' => 'was=Foo&wo=Bar'
+    """
+    search_filters = context.get("preserved_search_filters")
+
+    parsed_url = list(urlparse(url))
+    parsed_qs = parse_qsl(parsed_url[4])
+    merged_qs = []
+
+    if search_filters:
+        # Check if the url is for a search page. If it is, parse the preserved
+        # filters and extract the search filters parameters, so that they
+        # can be added to the query string. For example:
+        #  search_filters is:         '_search_filters=was%3DFoo%26wo%3DBar'
+        #  search_filters should be:  'was=Foo&wo=Bar'
+        search_filters = parse_qsl(search_filters)  # [('_search_filters', 'was=Foo&wo=Bar')]
+        match_url = f"/{unquote(url).partition(get_script_prefix())[2]}"
+        try:
+            match = resolve(match_url)
+        except Resolver404:  # pragma: no cover
+            pass
+        else:
+            filters = dict(search_filters).get("_search_filters")
+            if match.url_name == "suche" and filters:
+                search_filters = parse_qsl(filters)  # [('was', 'Foo'), ('wo', 'Bar')]
+
+        merged_qs.extend(search_filters)
+
+    merged_qs.extend(parsed_qs)
+
+    parsed_url[4] = urlencode(merged_qs)
+    return urlunparse(parsed_url)
--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -2,7 +2,10 @@ from urllib.parse import parse_qs, urlparse
 
 # noinspection PyPackageRequirements
 import pytest
-from jobby.templatetags.jobby import paginator_url
+from django.http import HttpResponse, QueryDict
+from django.urls import path, reverse
+from django.utils.http import urlencode
+from jobby.templatetags.jobby import add_search_filters, paginator_url
 from jobby.views import PAGE_VAR
 
 
@@ -11,3 +14,42 @@ def test_paginator_url(get_request, request_data):
     """Assert that ``paginator_url`` returns the expected URL."""
     url = paginator_url(context={"request": get_request}, page_number=1)
     assert parse_qs(urlparse(url).query) == {PAGE_VAR: ["1"], "foo": ["bar"]}
+
+
+def get_query_params(url):
+    """Return a dictionary of the query string parameters in the given URL."""
+    return QueryDict(urlparse(url).query)
+
+
+urlpatterns = [
+    path("suche/", lambda r: HttpResponse(), name="suche"),
+    path("not_suche/", lambda r: HttpResponse(), name="not_suche"),
+]
+
+
+@pytest.fixture
+def add_search_params_context():
+    return {"preserved_search_filters": urlencode({"_search_filters": urlencode([("was", "Foo"), ("wo", "Bar")])})}
+
+
+@pytest.mark.urls(__name__)
+def test_add_search_params_url_not_for_search_page(add_search_params_context):
+    """
+    Assert that the preserved search filters are added to the query string
+    under a special parameter if the url is not for the search page.
+    """
+    url = add_search_filters(add_search_params_context, reverse("not_suche"))
+    params = get_query_params(url)
+    assert params["_search_filters"] == "was=Foo&wo=Bar"
+
+
+@pytest.mark.urls(__name__)
+def test_add_search_params_url_for_search_page(add_search_params_context):
+    """
+    Assert that the preserved search filters are added to the query string
+    directly if the url is for the search page.
+    """
+    url = add_search_filters(add_search_params_context, reverse("suche"))
+    params = get_query_params(url)
+    assert params["was"] == "Foo"
+    assert params["wo"] == "Bar"
