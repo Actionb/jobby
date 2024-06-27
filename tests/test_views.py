10ce7df7bc52b3cb8574392a0aed630084205fa4
Actionb <yummytea1@gmail.com>
2024-06-27 13:19:18 +0200

    Set url conf for view tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -29,7 +29,23 @@ from jobby.views import (
 
 from tests.factories import StellenangebotFactory, WatchlistItemFactory
 
-pytestmark = [pytest.mark.django_db]
+urlpatterns = [
+    path("", lambda r: HttpResponse(), name="index"),
+    path("suche/", SucheView.as_view(), name="suche"),
+    path("merkliste/", WatchlistView.as_view(), name="watchlist"),
+    path("merkliste/toggle/", watchlist_toggle, name="watchlist_toggle"),
+    path("merkliste/remove/", watchlist_remove, name="watchlist_remove"),
+    path("merkliste/remove_all/", watchlist_remove_all, name="watchlist_remove_all"),
+    path("angebot/", StellenangebotView.as_view(extra_context={"add": True}), name="stellenangebot_add"),
+    path("angebot/<int:id>/", StellenangebotView.as_view(extra_context={"add": False}), name="stellenangebot_edit"),
+    path("fetch_description/<str:refnr>/", get_beschreibung, name="get_angebot_beschreibung"),
+    path("papierkorb/", PapierkorbView.as_view(), name="papierkorb"),
+    path("papierkorb/delete/", papierkorb_delete, name="papierkorb_delete"),
+    path("angebote/<int:id>/remove/", stellenangebot_remove, name="stellenangebot_remove"),
+    path("not_suche/", lambda r: HttpResponse(), name="not_suche"),
+]
+
+pytestmark = [pytest.mark.django_db, pytest.mark.urls(__name__)]
 
 
 @pytest.fixture
@@ -992,13 +1008,6 @@ class TestPapierkorbView:
         assert other_watchlist_item.stellenangebot not in queryset
 
 
-urlpatterns = [
-    path("suche/", lambda r: HttpResponse(), name="suche"),
-    path("not_suche/", lambda r: HttpResponse(), name="not_suche"),
-]
-
-
-@pytest.mark.urls(__name__)
 class TestBaseMixin:
 
     @pytest.fixture
